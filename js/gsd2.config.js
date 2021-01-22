7cc5901b05e65157c06ca55fadb1de882e51b4cf
acicchit <angelo.cicchitto@gmail.com>
2021-01-22 22:43:38 +0100

    Update gsd2.config.js

--- js/gsd2.config.js
+++ js/gsd2.config.js
@@ -6,28 +6,28 @@ window._genesys = {
           theme: 'dark'
       },
       webchat: {
-          form: {
-              wrapper: "<table></table>",
-              inputs: [
-                  {
-                      id: "cx_webchat_form_email",
-                      name: "email", 
-                      maxlength: "100",
-                      placeholder: "@i18n:webchat.ChatFormPlaceholderFirstName",
-                      label: "@i18n:webchat.ChatFormEmail",
-                      validateWhileTyping: false,
-                      validate: function(e, ndWrapper, ndInput, ndLabel, $, CXBus, Common){
-                        if(ndInput && ndInput.val() && ndInput.val().trim() != "" && ndInput.val().match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) return true;
-                          return false;
-                      },
-                  }
-              ]
-          },
-          autoInvite: {
-              enabled: true,
-              timeToInviteSeconds: 15,
-              inviteTimeoutSeconds: 30
-          },
+        form: {
+            wrapper: "<table></table>",
+            inputs: [
+                {
+                    id: "cx_webchat_form_email",
+                    name: "email", 
+                    maxlength: "100",
+                    placeholder: "@i18n:webchat.ChatFormPlaceholderFirstName",
+                    label: "@i18n:webchat.ChatFormEmail",
+                    validateWhileTyping: false,
+                    validate: function(e, ndWrapper, ndInput, ndLabel, $, CXBus, Common){
+                      if(ndInput && ndInput.val() && ndInput.val().trim() != "" && ndInput.val().match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) return true;
+                        return false;
+                    },
+                }
+            ]
+        },
+        autoInvite: {
+            enabled: true,
+            timeToInviteSeconds: 15,
+            inviteTimeoutSeconds: 30
+        },
       },
       sidebar: {
           showOnStartup: true,
@@ -52,11 +52,8 @@ var oPlugin = CXBus.registerPlugin("myPlugin");
 // Use before function to intercept 'WebChat.open' command
 // and manipulate the input 'options' object before execution continues
 oPlugin.before("WebChat.open", function(options){
-  
-  // We'll append an additional form field to the one already added 
-  // by 'open' command in myChatButton() function triggered by button
-  if(options)options.form.autoSubmit=true;
-  
+  //if(options)options.form.autoSubmit=true;
+  options.form.autoSubmit=true;
   // Still need to return the 'options' object to continue execution
   return options;
 });
