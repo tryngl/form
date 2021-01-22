a8ef3f02efe0e7e49527e4638e8e1bdbf3fa0dd6
acicchit <angelo.cicchitto@gmail.com>
2021-01-22 22:22:12 +0100

    gsd

--- js/gsd1.config.js
+++ js/gsd1.config.js
@@ -1,71 +1,50 @@
 window._genesys = {
-    widgets:{
-      webchat: {
-        form: {
-          wrapper: "<table></table>",
-          inputs: [
-            {
-              id: "cx_firstname",
-              name: "firstname",
-              placeholder: "@i18n:webchat.ChatFormPlaceholderFirstName",
-              label: "@i18n:webchat.ChatFormFirstName"
-            }
-          ]
-        }
+  widgets: {
+      main: {
+          debug: false,
+          preload: ['sidebar', 'webchat'],
+          theme: 'dark'
       },
-      onReady: function(){
-        CXBus.command('WebChat.open');
-      }
-    }
-  };
-
-  window._genesys = {
-    widgets: {
-        main: {
-            debug: false,
-            preload: ['sidebar', 'webchat'],
-						theme: 'dark'
-        },
-        webchat: {
-            form: {
-                wrapper: "<table></table>",
-                inputs: [
-                    {
-                        id: "cx_webchat_form_email",
-                        name: "email", 
-                        maxlength: "100",
-                        placeholder: "@i18n:webchat.ChatFormPlaceholderFirstName",
-                        label: "@i18n:webchat.ChatFormEmail",
-                        validateWhileTyping: false,
-                        validate: function(e, ndWrapper, ndInput, ndLabel, $, CXBus, Common){
-                          if(ndInput && ndInput.val() && ndInput.val().trim() != "" && ndInput.val().match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) return true;
-                            return false;
-                        },
-                    }
-                ]
-            },
-            autoInvite: {
-                enabled: true,
-                timeToInviteSeconds: 15,
-                inviteTimeoutSeconds: 30
-            },
-        },
-        sidebar: {
-            showOnStartup: true,
-            position: 'right',
-            expandOnHover: true,
-            channels:
-            [
-                {
-                name: 'WebChat',
-                clickCommand: 'WebChat.open', 
-                displayName: 'Chat', 
-                displayTitle: 'Get live help',
-                icon: 'chat'
-                }
-            ]
-        }    
-    }
+      webchat: {
+          form: {
+              wrapper: "<table></table>",
+              inputs: [
+                  {
+                      id: "cx_webchat_form_email",
+                      name: "email", 
+                      maxlength: "100",
+                      placeholder: "@i18n:webchat.ChatFormPlaceholderFirstName",
+                      label: "@i18n:webchat.ChatFormEmail",
+                      validateWhileTyping: false,
+                      validate: function(e, ndWrapper, ndInput, ndLabel, $, CXBus, Common){
+                        if(ndInput && ndInput.val() && ndInput.val().trim() != "" && ndInput.val().match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) return true;
+                          return false;
+                      },
+                  }
+              ]
+          },
+          autoInvite: {
+              enabled: true,
+              timeToInviteSeconds: 15,
+              inviteTimeoutSeconds: 30
+          },
+      },
+      sidebar: {
+          showOnStartup: true,
+          position: 'right',
+          expandOnHover: true,
+          channels:
+          [
+              {
+              name: 'WebChat',
+              clickCommand: 'WebChat.open', 
+              displayName: 'Chat', 
+              displayTitle: 'Get live help',
+              icon: 'chat'
+              }
+          ]
+      }    
+  }
 };
 
 // var widgetBaseUrl = 'https://apps.mypurecloud.com/widgets/9.0/',
